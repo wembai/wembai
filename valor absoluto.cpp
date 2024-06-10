450ce69168fec99b27037d75dc1cde5fd5dbcdbe
wembai <wenvazser07@gmail.com>
2024-06-09 23:27:31 -0600

    Add files via upload

---
+++ valor absoluto.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int valorAbsoluto(float num) 
+{
+if(num>0){
+	num-1
+	return num;}
+	else {
+		void;
+	}
+
+  }
+int main{
+	float num=0;
+	cout<<"ingresa un numero ";
+	cin>>num;
+	cout<<"tu valor absoluto es ";
+	cin>>num;
+    return 0;
+}
